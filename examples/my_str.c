04efaf42f3393428db0f77cbe9a358807cbf5fe3
Alex Alexandrov <sk.jame9@gmail.com>
2025-10-14 19:06:03 +0300

    up

--- examples/my_str.c
+++ examples/my_str.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
-
 int string2num(const char* string, unsigned long *out)
 {
-	unsigned long num = 0;
-	unsigned char digit; // переводиться как цифра
-	for(int i = 0; string[i] != 0; i++) {
-		digit = (string[i] - 0x30);
-		if (digit > 9)
-			return -1;
-		num = num * 10 + digit;
+	if (out == NULL)
+		return -1;
+
+	unsigned long num = 0; // initialize result. *out can be used without variable on stack
+	unsigned char digit;
+	for(int i = 0; string[i] != 0; i++) { // we consider that string has null-terminator
+		digit = (string[i] - 0x30); // Calc digit by decreasing on '0'
+		if (digit > 9) // check that digit boundaries not broken
+			return -1; // return with error
+		num = num * 10 + digit; // add 0 to the end of num and add digit after that
 	}
-	*out = num;
+	*out = num; // set pointer's value
 	return 0;
 }
 
