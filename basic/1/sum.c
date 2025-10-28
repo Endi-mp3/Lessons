2c49e480805ea8c9ff390bb9a0c0843e04f67465
Alex Alexandrov <sk.jame9@gmail.com>
2025-10-28 17:15:37 +0300

    upd

--- basic/1/sum.c
+++ basic/1/sum.c
@@ -2,45 +2,40 @@
 #include <string.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
+#define STR_A_MAX_LEN (4096 + 1)
+#define STR_B_MAX_LEN (2048 + 1)
 
-char a[4096];
-char b[2048];
-uint64_t Num1;
-uint64_t Num2;
-uint64_t Sum;
+char a[STR_A_MAX_LEN];
+char b[STR_B_MAX_LEN];
 
-
-int main() 
+bool readFile(const char* fpath, uint8_t* out, size_t maxOutLen)
 {
+	FILE *f = fopen(fpath, "r");
+    if (!f)
+		return false;
 
-FILE *f = fopen("a.txt", "r");
-    if(!f) {
-        return 1;
-    }
-         while (fgets(a,sizeof(a),f)) {
-            if (strncmp(a, "\0", 2) == 0){
-                break;
-            }
-         }
-        printf ("Txt1 = %s\n", a );
+	if (fgets(out, maxOutLen, f) == NULL) {
+		if (feof(f) == 0) {
+			return false;
+		}
+	}
+	fclose(f);
 
-        fclose(f);
+	return true;
+}
 
-FILE *h = fopen("b.txt", "r");
-    if(!h) {
-        return 1;
-    }
-         while (fgets(b,sizeof(b),h)) {
-            if (strncmp(b, "\0", 2) == 0){
-                break;
-            }
-         }
-           printf ("Txt2 = %s\n", b );
+int main(int argc, const char* argv[])
+{
+	if (argc < 4)
+		return -1;
 
-        fclose(h);  
+	if (!readFile(argv[1], a, STR_A_MAX_LEN))
+		return -2;
 
-
+	if (!readFile(argv[2], b, STR_B_MAX_LEN))
+		return -3;
 
 
 
