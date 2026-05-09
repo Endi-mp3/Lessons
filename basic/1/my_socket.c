aeced84a36f116d8ff6f1d6a561ab91bafd98f5e
Alex Alexandrov <sk.jame9@gmail.com>
2026-05-09 15:05:05 +0300

    add resource control helpers

--- basic/1/my_socket.c
+++ basic/1/my_socket.c
@@ -12,6 +12,33 @@
 #define DEFAULT_CONNECTION_CNT (10u)
 #define DEFAULT_MAX_LEN (4096u)
 
+inline void* my_malloc_fn(size_t size, const char* what, const char* where, int line)
+{
+	void* tmp_ptr = malloc(size);
+	printf("%s %i: malloc called %s[%i]. Result %x\n", where, line, what, size, (uintptr_t)tmp_ptr);
+	return tmp_ptr;
+}
+
+#define my_malloc(size, what) my_malloc_fn(size, what, __FUNCTION__, __LINE__)
+
+inline void my_free_fn(void* ptr, size_t size, const char* what, const char* where, int line)
+{
+	printf("%s %i: free called %s[%i] for %x\n", where, line, what, size, (uintptr_t)ptr);
+	free(ptr);
+}
+
+#define my_free(ptr, size, what) my_free_fn(ptr, size, what, __FUNCTION__, __LINE__)
+
+#define GOT_SOCKET(sock, what) printf("%s %i: got socket: %s fd %i\n", __FUNCTION__, __LINE__, what, sock)
+
+inline int my_close_fn(int sock, const char* what, const char* where, int line)
+{
+	printf("%s %i: close called %s fd=%i\n", where, line, what, sock);
+	return close(sock);
+}
+
+#define my_close(sock, what) my_close_fn(sock, what, __FUNCTION__, __LINE__)
+
 int port = 44004;
 const char server_ip[] = "127.0.0.1";
 bool flag_is_server = false, flag_is_clnt = false;
@@ -49,6 +76,7 @@ void print_package(const struct Packet* pkt, const uint8_t* pkt_data)
 int handle_server(int Max_len)
 {
 	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	GOT_SOCKET(sock, "server sock");
     if (sock < 0) {
         fprintf(stderr, "socket no open\n");
         return 1;
@@ -62,50 +90,48 @@ int handle_server(int Max_len)
 	int addrlen = sizeof(serv_addr);
 	if (bind(sock, (struct sockaddr*)&serv_addr, addrlen) < 0) {
         fprintf(stderr, "bind failed errno: %i\n", errno);
-		close(sock);
-
+		my_close(sock, "server sock");
         return 1;
     }
 	int connections_left = DEFAULT_CONNECTION_CNT;
 	do {
 		if (listen(sock, 1)) {
 			fprintf(stderr, "listen failed errno: %i\n", errno);
-			close(sock);
-
+			my_close(sock, "server sock");
 			return 1;
 		}
 		int client_socket = accept(sock, (struct sockaddr*)&serv_addr, (socklen_t*)&addrlen);
+		GOT_SOCKET(client_socket, "client socket");
 		if (client_socket < -1) {
-			close(sock);
-
-			return -1;
+			my_close(sock, "server sock");
+			continue;
 		}
 		uint8_t buf[sizeof(struct Header)] = { 0 };
 		ssize_t bytes_recv = -1;
 		bytes_recv = recv(client_socket, buf, sizeof(struct Header), 0);
 		if (bytes_recv == 0) {
-			close(client_socket);
+			my_close(client_socket, "client socket");
 			return -1;
 		} else if (bytes_recv == -1) {
-			close(client_socket);
+			my_close(client_socket, "client socket");
 			return -2;
 		}
 
 		struct Packet *pkt = (struct Packet *)buf;
 		if(pkt->header.len > Max_len) {
-			close(client_socket);
+			my_close(client_socket, "client socket");
 			return -1;
 		}
 
-		uint8_t *data_buf = (uint8_t *)malloc(pkt->header.len);
+		uint8_t *data_buf = (uint8_t *)my_malloc(pkt->header.len, "data_buf");
 		bytes_recv = recv(client_socket, data_buf, pkt->header.len, 0);
 		if (bytes_recv == 0) {
-			close(client_socket);
-			free(data_buf);
+			my_close(client_socket, "client socket");
+			my_free(data_buf, pkt->header.len, "data_buf");
 			return -1;
 		} else if (bytes_recv == -1) {
 			free(data_buf);
-			close(client_socket);
+			my_close(client_socket, "client socket");
 			return -2;
 		}
 		print_package(pkt, data_buf);
@@ -117,12 +143,12 @@ int handle_server(int Max_len)
 		p->header.cmd = pkt->header.cmd;
 		memcpy(p->data,	data_buf, pkt->header.len);
 		send(client_socket, p, sizeof(struct Packet) + p->header.len, 0);
-		close(client_socket);
+		my_close(client_socket, "client socket");
 		printf("paket otpravil");
 		free(p);
 
 	} while (connections_left--);
-	close(sock);
+	my_close(sock, "server sock");
 	return 0;
 }
 
@@ -135,6 +161,7 @@ int handle_clnt(const char *data_pkt)
     clnt_addr.sin_addr.s_addr = inet_addr(server_ip);
 
 	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	GOT_SOCKET(sock, "sock");
 	if (sock < 0)
 		return -1;
 
@@ -146,7 +173,7 @@ int handle_clnt(const char *data_pkt)
     }
     int connections_left = DEFAULT_CONNECTION_CNT;
     ssize_t data_pkt_len = strlen(data_pkt);
-    struct Packet *pkt = malloc(sizeof(struct Packet) + data_pkt_len);
+    struct Packet *pkt = my_malloc(sizeof(struct Packet) + data_pkt_len, "struct Packet* pkt");
     pkt->header.cmd = 0xA;
     pkt->header.id  = 0xDEAD;
     pkt->header.len = data_pkt_len;
