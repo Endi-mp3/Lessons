9460796844c99752d69f15722c468cf26a0b5324
Andre <@email.com>
2026-09-04 20:28:49 +0300

    Added some command processing for esp

--- embedded/esp32/ir/irw_raw.c
+++ embedded/esp32/ir/irw_raw.c
@@ -1,11 +1,16 @@
 #include "irw.h"
 #include "esp_log.h"
 #include "driver/rmt_rx.h"
-
+#include "my_socket_proto.h"
+#include "my_socket_lib.h"
+#include <stdbool.h>
 static const char *TAG = "IR_FILTER";
 
 #define IR_GLITCH_FILTER_NS  (200000)   /* Игнорировать импульсы короче 200 мкс (типичный мусор) */
 #define MIN_SIGNAL_SYMBOLS   (10)       /* Игнорировать пакеты короче 10 перепадов (случайные всплески) */
+#define MAX_SLOT (10)
+uint8_t slot_ir_signal[MAX_SLOT]
+uint8_t slot_ir_status[MAX_SLOT]
 
 rmt_channel_handle_t ir_raw_rx_init(int gpio_num)
 {
@@ -79,4 +84,65 @@ esp_err_t ir_raw_send(rmt_channel_handle_t tx_chan, ir_raw_packet_t *data)
 		.loop_count = 0
 	};
     return rmt_transmit(tx_chan, data->symbols, data->length, &tx_cfg);
+}
+
+
+main handler_packet_cmd(int sock, struct Packet *pkt)
+{
+	switch(pkt->header.cmd)
+	{
+		case my_sock_cmd_get:
+		
+		
+		
+		case my_sock_cmd_set:
+		
+		
+		case my_sock_cmd_err:
+		
+		case my_sock_cmd_watcher_settings:
+		
+		case my_sock_cmd_full_reset:
+		
+			memset(slot_ir_signal, 0, sizeof(slot_ir_signal));
+            memset(slot_ir_status, 0, sizeof(slot_ir_status));
+            my_sock_send(sock, pkt->header.id, my_sock_cmd_info, 1, slot_ir_signal);
+            break;
+            
+		case my_sock_cmd_slot_clean:
+		
+		uint8_t target_slot = pkt->data[0];
+		
+		if (target_slot >= MAX_SLOT)
+		{
+			break;
+		} else
+		
+		{
+				slot_ir_signal[target_slot] = 0;
+				slot_ir_status[target_slot] = 0;
+		}
+		my_sock_send(sock, pkt->header.id, my_sock_cmd_info, 1, slot_ir_signal);
+		break;
+		
+			
+		
+		
+		case my_sock_cmd_triger_menu:
+		
+            
+        for (int i = 0; i < MAX_SLOT; i++) 
+		{
+			if (slot_ir_status[i] == 1)
+			{
+				slot_ir_signal[i] = 1;
+			} 
+			else 
+			{
+				slot_ir_signal[i] = 0;
+			}
+		}
+		my_sock_send(sock, pkt->header.id, my_sock_cmd_info, MAX_SLOT, slot_ir_signal);
+    break;
+	}
 }
